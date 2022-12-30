c6c52e6e821b7d95947be5461034bbbd337be975
zeroboy <tranhuudang148@gmail.com>
2022-12-30 08:52:27 +0700

    Initial Commit

---
+++ Kudo/KudoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KudoApp.swift
+//  Kudo
+//
+//  Created by Tran Huu Dang on 30/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct KudoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
